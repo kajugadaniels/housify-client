2ea62019e8209a0382b5fba64571030cd90e1e2e
unknown <kajugadaniels@gmail.com>
2025-10-17 19:48:03 +0200

    Setup shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import { Button } from "./components/ui/button"
 
-const App = () => {
+function App() {
 	return (
-		<div>App</div>
+		<div className="flex min-h-svh flex-col items-center justify-center">
+			<Button>Click me</Button>
+		</div>
 	)
 }
 
